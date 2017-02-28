ef9ebdf075ed74aa89ecc82555163f214f643881
Mike Botsko <botsko@gmail.com>
2017-02-28 08:56:41 -0800

    Base entity class and component interfaces

---
+++ OmniAPI/Components/ILootComponent.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * This file is part of OmniAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2017 Helion3 http://helion3.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using System.Collections.Generic;
+
+namespace OmniAPI {
+	/// <summary>
+	/// Represents an entity's loot drops.
+	/// </summary>
+	public interface ILootComponent {
+		/// <summary>
+		/// Add a loot item with a 100% drop chance.
+		/// </summary>
+		/// <typeparam name="IItem">The item type.</typeparam>
+		void Add<T>(int quantity = 1) where T : IItem;
+
+		/// <summary>
+		/// Add a loot item with given drop chance.
+		/// 
+		/// Note: currently does not support quantities.
+		/// </summary>
+		/// <param name="weight">Weight.</param>
+		/// <typeparam name="T">The item type.</typeparam>
+		void AddWeighted<T>(int weight) where T : IItem;
+
+		/// <summary>
+		/// Get drops. Will give priority to any non-weighted loot.
+		/// If none exists, a weighted loot item will be returned.
+		/// </summary>
+		/// <returns>The drops.</returns>
+		List<IItem> Drops();
+	}
+}
---
+++ OmniAPI/Entities/Entity.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * This file is part of OmniAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2017 Helion3 http://helion3.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using UnityEngine;
+
+namespace OmniAPI {
+	abstract public class Entity : MonoBehaviour, IEntity {
+		// An ID of the current prefab
+		public string PrefabId;
+
+		// A unique UUID assigned to this specific entity
+		public readonly string UniqueID = System.Guid.NewGuid().ToString();
+
+		// A convenience property caching our parent tile, if any
+		public ITile Tile;
+
+		/// <summary>
+		/// Gets the identifier.
+		/// </summary>
+		/// <returns>The identifier.</returns>
+		public virtual string GetId() {
+			return null;
+		}
+
+		/// <summary>
+		/// Get the current world position, floored to the current tile coordinate.
+		/// </summary>
+		/// <returns>The world position.</returns>
+		public Vector3 GetWorldPosition() {
+			var pos = transform.position;
+
+			// Round, because tiles are positioned via the center pivot, so tile 2,0 includes 1.5,0 - 2.5,0
+			return new Vector3(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
+		}
+
+		/// <summary>
+		/// Called when the entity is broken by a game object or player.
+		/// </summary>
+		public virtual void OnBreak() {}
+	}
+}
